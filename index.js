7bb9ef707ebc03a55dded751932f26ee1b090200
unknown <kayajohnson71@yahoo.com>
2020-08-19 12:57:15 -0400

    Added express requirement

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const db = require('./queries')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./queries');
+const cors = require('cors');
+const { response } = require('express');
+const app = express();
 const port = 3000
-const cors = require('cors')
-app.use(cors())
 
-app.use(bodyParser.json())
+app.use(cors());
+
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
